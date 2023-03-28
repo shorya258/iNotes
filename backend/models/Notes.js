79d47d8d4519a31f8b5d37cbe0725e126ea8b733
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-03-28 14:34:16 +0530

    added notes route

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const NotesSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
   name: {
     type: String,
     required: true,
@@ -11,7 +15,7 @@ const NotesSchema = new Schema({
   },
   tag: {
     type: String,
-    default: general,
+    default: "General",
   },
   date: {
     type: Date,
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//Route 1: Fetch all notes using: POST "api/auth/fetchallnotes". Login required
+// header me se token lena hai
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
 });
+//Route 2: Add a new note using: POST "api/auth/addnotes". Login required
+router.post(
+  "/addnote",
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "description must be at least 5 characters").isLength({
+      min: 3,
+    }),
+  ],
+  fetchuser,
+  async (req, res) => {
+    const { title, description, tag } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const note = new Note({ title, description, tag, user: req.user.id });
+    // const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  }
+);
 module.exports = router;
